2d0bdee938b836d8158e318169590c93d64ff774
eloven <guobin201314@gmail.com>
2018-12-05 09:25:34 +0800

    包放在内部

--- basejava/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author GuoBin201314@gmail.com on 2018/11/15 21:58
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello World");
-    }
-}
