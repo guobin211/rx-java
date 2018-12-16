ea097dc41a39d67ddbe67291476198e3827b44e1
eloven <guobin201314@gmail.com>
2018-12-16 09:19:24 +0800

    数组栈测试

---
+++ src/stack/Main.java
@@ -0,0 +1,23 @@
+package stack;
+
+/**
+ * FileName Main.java
+ *
+ * @author guobin201314@gmail.com on 2018/12/8 18:59
+ * @version 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+
+        // 继承 Array ===> ArrayStack ===> Object
+        System.out.println(arrayStack);
+
+        for (int i = 0; i < 5; i++) {
+            arrayStack.push(i);
+            System.out.println(arrayStack);
+        }
+
+        System.out.println(arrayStack.peek());
+    }
+}
