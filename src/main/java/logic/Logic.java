9f39694f026467e92ed5ec60e53ba708f5ff8ea5
eloven <guobin201314@gmail.com>
2019-01-13 22:01:31 +0800

    logic

---
+++ src/main/java/logic/Logic.java
@@ -0,0 +1,20 @@
+package logic;
+
+/**
+ * @author GuoBin201314@gmail.com on 2018/11/17 11:48
+ */
+public class Logic {
+    public static void main(String[] args) {
+        String test;
+        test = "hello";
+        isNull(test);
+    }
+    private static void isNull(String s) {
+        String test = "hello";
+        if (test.equals(s)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
