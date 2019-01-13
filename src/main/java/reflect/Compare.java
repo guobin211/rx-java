2be257c9687b5e568ee2b4793026cc2fd7457632
eloven <guobin201314@gmail.com>
2019-01-13 21:59:47 +0800

    reflect

---
+++ src/main/java/reflect/Compare.java
@@ -0,0 +1,30 @@
+package reflect;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author GuoBin201314@gmail.com on 2018/11/17 23:30
+ */
+public class Compare {
+    public static void main(String[] args) {
+        String[] strings;
+        strings = new String[]{"A", "C", "B", "b", "c"};
+        Arrays.sort(strings, Comparator.naturalOrder());
+        //  使用内置方法排序
+        Arrays.sort(strings, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(strings));
+        lambda(strings);
+    }
+
+    /**
+     * 箭头函数 排序
+     * @param s []
+     */
+    static void lambda(String[] s) {
+        Arrays.sort(s, (s1, s2) -> {
+            return s1.toLowerCase().compareTo(s2.toLowerCase());
+        });
+        System.out.println(Arrays.toString(s));
+    }
+}
