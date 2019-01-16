4f5073fc6b642aa2a4f6cae311721c8fd1f44699
eloven <guobin201314@gmail.com>
2019-01-16 14:46:48 +0800

    read json

--- src/main/java/lambda/FunctionDemo.java
+++ src/main/java/lambda/FunctionDemo.java
@@ -11,12 +11,11 @@ import java.util.function.Predicate;
  */
 public class FunctionDemo {
     public static void main(String[] args) {
-        // 断言
+        // 断言 接收T对象并返回boolean
         Predicate<Integer> predicate = i -> i > 0;
         System.out.println(predicate.test(-6));
-
+        // 接收T对象，不返回值
         Consumer<String> consumer = s -> System.out.println("消费了" + s);
-
         consumer.accept("consumer");
     }
 }
--- src/main/java/math/String.java
+++ src/main/java/math/String.java
@@ -3,7 +3,7 @@ package math;
 /**
  * @author GuoBin201314@gmail.com on 2018/11/16 11:40
  */
-public class String {
+class String {
     public static void main(String[] args) {
 
     }
