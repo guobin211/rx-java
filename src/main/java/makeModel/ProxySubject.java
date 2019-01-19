de0e8df0444a9b6137d740d199addbe89771991a
eloven <guobin201314@gmail.com>
2019-01-19 20:43:48 +0800

    代理模式

---
+++ src/main/java/makeModel/ProxySubject.java
@@ -0,0 +1,40 @@
+package makeModel;
+
+interface Meeting {
+    void call();
+}
+
+class Boos implements Meeting {
+
+    @Override
+    public void call() {
+        System.out.println("Boos call Meeting");
+    }
+}
+
+/**
+ * 秘书代理Boos
+ */
+class Secretary implements Meeting {
+    Meeting meeting = new Boos();
+    @Override
+    public void call() {
+        System.out.println("Secretary call Meeting");
+        meeting.call();
+    }
+}
+
+/**
+ * makeModel   ProxySubject
+ *
+ * @author guobin201314@gmail.com on 2019-01-19
+ * @version 1.0.0 by JDK 1.8
+ */
+public class ProxySubject {
+    public static void main(String[] args) {
+        System.out.println("代理模式");
+
+        Meeting meeting = new Secretary();
+        meeting.call();
+    }
+}
