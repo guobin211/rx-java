057b9ce98a1e6afe3666c8c7ccb6e47ff4bf1950
eloven <guobin201314@gmail.com>
2019-01-24 20:46:22 +0800

    hash set

--- src/main/java/collection/SelfSet.java
+++ src/main/java/collection/SelfSet.java
@@ -10,6 +10,35 @@ public class SelfSet {
         List<String> list = Arrays.asList("jack", "tom", "tom", "mary");
         List<String> resList = removeDuplicate(list);
         System.out.println(resList);
+
+
+        // hashSet 继承 自 set , 内部无重复元素, 无索引, 存取顺序不一致
+
+        HashSet<String> hashSet = new HashSet<String>(Arrays.asList("jack", "tom", "tom", "mary"));
+        System.out.println(hashSet);
+
+        hashSet.add("hock");
+        System.out.println(hashSet);
+        // for 遍历
+        for (String hs: hashSet) {
+            System.out.println(hs);
+        }
+
+        // 针对自定义object 需要重写equals, hashCode
+        HashSet<SelfPerson> selfPersonHashSet = new HashSet<>();
+        SelfPerson selfPerson = new SelfPerson("张飞", 22);
+        selfPersonHashSet.add(selfPerson);
+        selfPersonHashSet.add(new SelfPerson("张飞", 18));
+        selfPersonHashSet.add(new SelfPerson("张飞", 18));
+        selfPersonHashSet.add(new SelfPerson("赵云", 18));
+        selfPersonHashSet.add(new SelfPerson("赵云", 18));
+
+        for (SelfPerson person: selfPersonHashSet) {
+            System.out.println(person.toString());
+        }
+
+        System.out.println(selfPersonHashSet);
+
     }
 
     /**
