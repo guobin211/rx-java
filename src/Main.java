d1a95dfa7dcfda43f0fba1a7a08e621acacf3543
guobin <844922881@qq.com>
2019-05-24 17:30:39 +0800

    增加
    sring 子目录模块

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import string.StringUtils;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        StringUtils.getName();
     }
 }
--- src/string/Util.java
+++
@@ -1,9 +0,0 @@
-package string;
-
-/*
-* Util
-* @Author guobin201314@gmail.com on
-*/
-
-public class Util {
-}
