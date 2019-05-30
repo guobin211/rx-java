614cf3fad4b4f003e3cc797fde8005ed3c9ff64c
GuoBin <844922881@qq.com>
2019-05-30 23:27:07 +0800

    dip
    依赖导致原则

---
+++ src/a_oop_dip/Book.java
@@ -0,0 +1,15 @@
+package a_oop_dip;
+
+/**
+ * Book
+ *
+ * @author GuoBin211 on 2019-05-30 22:50
+ */
+class Book implements IReader {
+    @Override
+    public void getContent() {
+        System.out.println("获取Book的内容: getContent");
+    }
+
+}
+
