39536996eb28b24e95ed49ab517f16b9c2192d02
GuoBin <844922881@qq.com>
2019-05-30 23:26:40 +0800

    isp
    接口隔离原则

---
+++ src/a_oop_isp/Main.java
@@ -0,0 +1,19 @@
+package a_oop_isp;
+
+/**
+ * Main
+ * 接口隔离原则
+ * @author GuoBin211 on 2019-05-30 23:04
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("isp ");
+        // search 查找多个属性
+        Searcher searcher = new Searcher();
+        searcher.searchGirlPretty(new OneGirl());
+        // 查找单个属性
+        SearchVoice searchVoice = new SearchVoice();
+        searchVoice.searchVoiceGirl(new VoiceGirl());
+    }
+}
+
