0b2e4b7872e9fae5387818be5e0129c62f159ea8
GuoBin <844922881@qq.com>
2019-05-31 23:36:04 +0800

    工厂方法

---
+++ src/a_design_create/factory_method/KfcStore.java
@@ -0,0 +1,16 @@
+/*
+ * a_design_create.factory_method KfcStore
+ * @author GuoBin211 on 2019-05-31 22:36
+ */
+package a_design_create.factory_method;
+
+public class KfcStore implements IStore {
+    KfcStore() {
+    }
+    @Override
+    public IChips getChips() {
+        // 简单工厂
+        return new KfcChips();
+    }
+}
+
