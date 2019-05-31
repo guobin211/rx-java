7a1b309d7d69b1a280ac2d4863ccb5d8e78dc1c9
GuoBin <844922881@qq.com>
2019-05-31 23:36:23 +0800

    代理模式

---
+++ src/a_design_create/proxy/USASeller.java
@@ -0,0 +1,13 @@
+/*
+ * a_design_create.proxy USASeller
+ * @author GuoBin211 on 2019-05-31 22:58
+ */
+package a_design_create.proxy;
+
+public class USASeller implements IMacSeller {
+    @Override
+    public void sell() {
+        System.out.println("USASeller sell Mac...");
+    }
+}
+
