d456379be59470e2c24c642e38e33822c7b140c9
GuoBin <844922881@qq.com>
2019-05-31 23:36:34 +0800

    简单工厂

---
+++ src/a_design_create/simple_factory/Main.java
@@ -0,0 +1,35 @@
+/*
+ * a_design_create.simple_factory Main
+ * @author GuoBin211 on 2019-05-31 20:07
+ */
+package a_design_create.simple_factory;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("简单工厂模式");
+        FoodsShop foodsShop = new FoodsShop();
+
+        IFood iFood = foodsShop.getFood("Fish");
+        if (iFood != null) {
+            iFood.eat();
+        }
+
+        iFood = foodsShop.getFood("Meat");
+        if (iFood != null) {
+            iFood.eat();
+        }
+
+        iFood = foodsShop.getFood("Tomato");
+        if (iFood != null) {
+            iFood.eat();
+        }
+        try {
+            iFood.eat();
+        } catch (NullPointerException e) {
+            // null
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
+
