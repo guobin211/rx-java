de5d42cecd2a4724d16382f3d8cac2e2885e8ece
GuoBin <844922881@qq.com>
2019-06-01 10:41:58 +0800

    策略模式

---
+++ src/a_design_structure/strategy/AskPerson.java
@@ -0,0 +1,18 @@
+/*
+ * a_design_structure.strategy AskPerson
+ * @author GuoBin211 on 2019-06-01 10:33
+ */
+package a_design_structure.strategy;
+
+class AskPerson {
+    private IStrategy strategy;
+    AskPerson(IStrategy strategy) {
+        this.strategy = strategy;
+    }
+    void askPeople() {
+        // 问问题获得回答
+        System.out.println("AskPerson 问策略");
+        strategy.getStrategy();
+    }
+}
+
