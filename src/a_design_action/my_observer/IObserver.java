b316c7acbb3085df4d4aee6d5353d2fc6dc5dba7
GuoBin <844922881@qq.com>
2019-06-01 10:41:24 +0800

    观察者模式

---
+++ src/a_design_action/my_observer/IObserver.java
@@ -0,0 +1,9 @@
+/*
+ * a_design_action.my_observer IObserver
+ * @author GuoBin211 on 2019-06-01 09:06
+ */
+package a_design_action.my_observer;
+
+public interface IObserver {
+    void update(MyPublishData myPublishData);
+}
