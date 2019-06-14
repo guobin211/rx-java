6a356770232220e2991c4d2ab3aa95bc871d6829
GuoBin <844922881@qq.com>
2019-06-14 22:20:30 +0800

    JS Array

---
+++ src/array/IJsArray.java
@@ -0,0 +1,25 @@
+package array;
+
+/*
+ * IJsArray
+ * @Author guobin201314@gmail.com on 2019-06-12 22:44
+ */
+public interface IJsArray<E> {
+    int getSize();
+    int getCapacity();
+    boolean isEmpty();
+    E shift();
+    E pop();
+    E get(int i);
+    void unshift(E e);
+    void push(E e);
+    IJsArray reverse();
+    void slice();
+    void splice();
+    IJsArray concat(IJsArray e);
+    int indexOf(E e);
+    boolean includes(E e);
+    void forEach();
+    void map();
+    void flatMap();
+}
