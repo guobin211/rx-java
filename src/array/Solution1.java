e66ea898738edcf53bbe41b5d999244ca61e7e22
GuoBin <844922881@qq.com>
2019-06-19 18:05:41 +0800

    链表和递归算法

--- src/array/Solution1.java
+++ src/array/Solution1.java
@@ -1,24 +1,37 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Solution1 class
  *
- * @author GuoBin
+ * @author GuoBin 杨辉三角
  * @email guobin201314@gmail.com
  * @date 2019-06-19
  */
 public class Solution1 {
-    public int[] findDiagonalOrder(int[][] matrix) {
-        int m,n;
-        m = matrix.length;
-        n = matrix[0].length;
-        if (m == 0 || n == 0) {
-            return new int[0];
-        }
-        int[] res = new int[m * n];
-        for (int i = 0; i < m * n; i++) {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
 
+        for (int i = 0; i < numRows; i++) {
+            List son = new ArrayList();
+            for (int j = 0; j <= i ; j++) {
+                if (j == 0|| j == i){
+                    son.add(1);
+                } else {
+                    Integer a = res.get(i - 1).get(j);
+                    Integer b = res.get(i - 1).get(j - 1);
+                    son.add(a + b);
+                }
+            }
+            res.add(son);
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        List res = new Solution1().generate(5);
+        System.out.println(res);
     }
 }
--- src/linklist/Solution3.java
+++ src/linklist/Solution3.java
@@ -9,8 +9,20 @@ public class Solution3 {
         if (head == null || head.next == null) {
             return head;
         } else {
+            // dummy->1(l1)->2(l2)->3(nextStart)->4->5->6
             ListNode dummy = new ListNode(-1);
-            dummy.next = head;
+            ListNode l1 = dummy;
+            ListNode l2 = head;
+            ListNode nextStart;
+
+            while (l2 != null && l2.next!=null) {
+                nextStart = l2.next.next;
+                l1.next = l2.next;
+                l2.next.next = l2;
+                l2.next = nextStart;
+                l1=l2;
+                l2=l2.next;
+            }
 
             return dummy.next;
         }
