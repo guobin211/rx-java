cd915a20366fca127fb328e5de5f4cd5e1e56528
GuoBin <guobin201314@gmail.com>
2019-08-24 19:21:01 +0800

    android

---
+++ array/src/main/java/com/guobin/array/Array.java
@@ -0,0 +1,7 @@
+package com.guobin.array;
+
+public class Array {
+    private Object[] data;
+    private int length;
+    private boolean isEmpty;
+}
---
+++ array/src/main/java/com/guobin/array/Main.java
@@ -0,0 +1,12 @@
+package com.guobin.array;
+
+/**
+ * Main
+ *
+ * @author GuoBin on 2019-08-24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
