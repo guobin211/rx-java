94c9abc6fbf1736e6961c4ed33e786fa7a92392f
GuoBin <guobin201314@gmail.com>
2019-08-31 11:02:39 +0800

    grid-view

--- layout/app/src/main/java/com/guobin/layout/MainActivity.java
+++ layout/app/src/main/java/com/guobin/layout/MainActivity.java
@@ -11,23 +11,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.guobin.layout.activity.BaseAdapterActivity;
+import com.guobin.layout.activity.GridViewActivity;
 import com.guobin.layout.activity.LinerLayout;
-import com.guobin.layout.activity.ListView;
+import com.guobin.layout.activity.ListViewActivity;
+import com.guobin.layout.activity.SimpleAdapterActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1;
     Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1 = findViewById(R.id.button1);
-        btn1.setOnClickListener(this.onListViewClick(0));
+        btn1.setOnClickListener(this.onListViewClick(1));
         btn2 = findViewById(R.id.button2);
-        btn2.setOnClickListener(this.onListViewClick(1));
+        btn2.setOnClickListener(this.onListViewClick(2));
+        btn3 = findViewById(R.id.button3);
+        btn3.setOnClickListener(this.onListViewClick(3));
+        btn4 = findViewById(R.id.button4);
+        btn4.setOnClickListener(this.onListViewClick(4));
+        btn5 = findViewById(R.id.button5);
+        btn5.setOnClickListener(this.onListViewClick(5));
     }
 
     @Override
@@ -53,12 +65,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                if (id == 0) {
-                    intent.setClass(MainActivity.this, ListView.class);
-                } else if (id == 1) {
+                if (id == 1) {
+                    intent.setClass(MainActivity.this, ListViewActivity.class);
+                } else if (id == 2) {
                     intent.setClass(MainActivity.this, LinerLayout.class);
+                } else if (id == 3) {
+                    intent.setClass(MainActivity.this, SimpleAdapterActivity.class);
+                } else if (id == 4) {
+                    intent.setClass(MainActivity.this, BaseAdapterActivity.class);
+                } else if (id == 5) {
+                    intent.setClass(MainActivity.this, GridViewActivity.class);
                 } else {
-                    intent.setClass(MainActivity.this, ListView.class);
+                    intent.setClass(MainActivity.this, ListViewActivity.class);
                 }
                 startActivity(intent);
             }
