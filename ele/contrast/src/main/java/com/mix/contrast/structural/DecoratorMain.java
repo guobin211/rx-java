54cfbbe4a891195a3cbc86d05f03a1b4935b3dc7
GuoBin <guobin201314@gmail.com>
2019-11-13 10:28:10 +0800

    装饰模式

---
+++ ele/contrast/src/main/java/com/mix/contrast/structural/DecoratorMain.java
@@ -0,0 +1,77 @@
+package com.mix.contrast.structural;
+
+interface DomElement {
+    /**
+     * render()
+     */
+    void render();
+}
+
+class DivElement implements DomElement {
+
+    @Override
+    public void render() {
+        System.out.println("DivElement render()");
+    }
+}
+
+class SpanElement implements DomElement {
+
+    @Override
+    public void render() {
+        System.out.println("SpanElement render()");
+    }
+}
+
+abstract class BaseDocument implements DomElement {
+
+    private DomElement element;
+
+    BaseDocument(DomElement domElement) {
+        element = domElement;
+    }
+
+    @Override
+    public void render() {
+        element.render();
+    }
+}
+
+class Document extends BaseDocument {
+
+    private int random = 0;
+
+    Document(DomElement domElement) {
+        super(domElement);
+    }
+
+    @SuppressWarnings("AlibabaUndefineMagicConstant")
+    @Override
+    public void render() {
+        super.render();
+        if (this.random % 2 == 0) {
+            this.setColor("#ffffff");
+        } else {
+            this.setColor("#cccccc");
+        }
+        this.random++;
+    }
+
+    private void setColor(String color) {
+        System.out.println("Document.color = " + color);
+    }
+}
+
+/**
+ * @author guobin211
+ * @version 0.0.1
+ * @date 2019-11-13  10:08
+ */
+public class DecoratorMain {
+    public static void main(String[] args) {
+        System.out.println("装饰器模式");
+        Document document = new Document(new DivElement());
+        document.render();
+        document.render();
+    }
+}
